63c84589fb0527d2978c42fba0d196907f636fdb
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-22 16:42:53 -0400

    first commit

---
+++ src/test/java/com/example/coursemanagerserver/CourseManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.coursemanagerserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
