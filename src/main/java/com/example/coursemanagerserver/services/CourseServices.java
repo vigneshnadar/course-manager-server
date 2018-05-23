b5511c9e9ba5337c71119bba3e9d0a631b0189eb
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-23 06:27:13 -0400

    course service create course

--- src/main/java/com/example/coursemanagerserver/services/CourseServices.java
+++ src/main/java/com/example/coursemanagerserver/services/CourseServices.java
@@ -1,19 +1,31 @@
 package com.example.coursemanagerserver.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.coursemanagerserver.models.Course;
 import com.example.coursemanagerserver.repositories.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge =3600)
 public class CourseServices {
 	@Autowired
 	CourseRepository courseRepository;	
+	
 	@GetMapping("/api/course")
 	public Iterable<Course> findAllCourses() {
 		return courseRepository.findAll(); 
 	}
+	
+	@PostMapping("/api/course")
+	public Course createCourse
+	(@RequestBody Course course) {
+			return courseRepository.save(course);
+	}
+
 }
 
