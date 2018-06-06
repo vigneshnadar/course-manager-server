de9484487c79d82937633894b37870dc331c42d7
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-06 11:44:53 -0400

    added exam service

--- src/main/java/com/example/coursemanagerserver/services/ExamService.java
+++ src/main/java/com/example/coursemanagerserver/services/ExamService.java
@@ -1,16 +1,19 @@
 package com.example.coursemanagerserver.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.coursemanagerserver.models.Assignment;
 import com.example.coursemanagerserver.models.Exam;
 import com.example.coursemanagerserver.models.Lesson;
 import com.example.coursemanagerserver.models.MultipleChoiceQuestion;
@@ -32,9 +35,84 @@ public class ExamService {
 	@Autowired
 	ExamRepository examRepository;
 	@Autowired
+	LessonRepository lessonRepository;
+	@Autowired
 	TrueFalseQuestionRepository trueFalseRepository;
 	@Autowired
 	MultipleChoicesQuestionRepository mutiRepo;
+	
+	
+	@GetMapping("/api/exam")
+	public List<Exam> findAllExams(){
+		
+		return (List<Exam>) examRepository.findAll();
+		
+	}
+	
+	
+	 @GetMapping("/api/exam/{id}")
+		public Exam findExamById(@PathVariable("id") int id) {
+			Optional<Exam> data = examRepository.findById(id); 
+			
+			if(data.isPresent())
+				return data.get();
+			
+			return null;
+			
+		}
+	 
+	 @DeleteMapping("/api/exam/{examId}")
+		public void deleteExam(
+		@PathVariable("examId") int id) {
+		 examRepository.deleteById(id);
+		}
+	 
+	 
+
+		@GetMapping("/api/lesson/{lessonId}/exam")
+		public List<Widget> findAllExamsForLesson(@PathVariable("lessonId") int lessonId) {
+			System.out.println("here");
+			Optional<Lesson> optionalLesson = lessonRepository.findById(lessonId);
+			
+			List<Widget> outputWidgets = new ArrayList<Widget>();
+			if(optionalLesson.isPresent()) {
+				Lesson lesson = optionalLesson.get();
+				List<Widget> widgets = lesson.getWidgets();
+				
+				
+				for(Widget w: widgets) {
+					if(w.getWidgetType().equals("Assignment")) {
+						outputWidgets.add(w);
+					}
+				}
+				int count = outputWidgets.size();
+				return outputWidgets;
+			}
+			return null;
+		}
+		
+		
+		@PostMapping("/api/lesson/{lessonId}/exam")
+		public void createWidgets(@PathVariable("lessonId") int lessonId, @RequestBody List<Exam> exams) {
+			Optional<Lesson> data = lessonRepository.findById(lessonId);
+			
+
+			if (data.isPresent()) {
+				Lesson les = data.get();
+				List<Widget> prevWidgets = les.getWidgets();
+				
+//				for(Widget widget: prevWidgets){
+//					 repository.deleteById(widget.getId());
+//				}
+//				
+//				repository.deleteAll();
+				for(Exam ex: exams){
+					ex.setLesson(les);
+					examRepository.save(ex);
+				}
+			}
+//			return null;
+		}
 
 	@GetMapping("/api/multi/{questionId}")
 	public MultipleChoiceQuestion findMultiQuestionById(@PathVariable("questionId") int questionId) {
@@ -44,6 +122,11 @@ public class ExamService {
 		}
 		return null;
 	}
+	
+	
+	
+	
+	
 
 	@GetMapping("/api/truefalse/{questionId}")
 	public TrueFalseQuestion findTrueFalseQuestionById(@PathVariable("questionId") int questionId) {
