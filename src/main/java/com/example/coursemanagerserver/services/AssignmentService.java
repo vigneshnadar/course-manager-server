a6d6fd047fee3c2e6c1a08611efa457bb254e1a2
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-06 11:20:00 -0400

    assignment api

--- src/main/java/com/example/coursemanagerserver/services/AssignmentService.java
+++ src/main/java/com/example/coursemanagerserver/services/AssignmentService.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.coursemanagerserver.models.Assignment;
+import com.example.coursemanagerserver.models.Course;
 import com.example.coursemanagerserver.models.Exam;
 import com.example.coursemanagerserver.models.Lesson;
 import com.example.coursemanagerserver.models.MultipleChoiceQuestion;
@@ -20,6 +23,7 @@ import com.example.coursemanagerserver.models.Question;
 import com.example.coursemanagerserver.models.TrueFalseQuestion;
 
 import com.example.coursemanagerserver.models.Widget;
+import com.example.coursemanagerserver.repositories.AssignmentRepository;
 import com.example.coursemanagerserver.repositories.ExamRepository;
 import com.example.coursemanagerserver.repositories.LessonRepository;
 import com.example.coursemanagerserver.repositories.MultipleChoicesQuestionRepository;
@@ -34,7 +38,7 @@ public class AssignmentService {
 	@Autowired
 	LessonRepository lessonRepository;
 	@Autowired
-	TrueFalseQuestionRepository trueFalseRepository;
+	AssignmentRepository assignRepository;
 	@Autowired
 	MultipleChoicesQuestionRepository mutiRepo;
 
@@ -57,6 +61,32 @@ public class AssignmentService {
 //		return null;
 //	}
 	
+	
+	@GetMapping("/api/assignment")
+	public List<Assignment> findAllAssignments(){
+		
+		return (List<Assignment>) assignRepository.findAll();
+		
+	}
+	
+	
+	 @GetMapping("/api/assignment/{id}")
+		public Assignment findAssignmentById(@PathVariable("id") int id) {
+			Optional<Assignment> data = assignRepository.findById(id); 
+			
+			if(data.isPresent())
+				return data.get();
+			
+			return null;
+			
+		}
+	 
+	 @DeleteMapping("/api/assignment/{assignmentId}")
+		public void deleteAssignment(
+		@PathVariable("assignmentId") int id) {
+		 assignRepository.deleteById(id);
+		}
+	
 	@GetMapping("/api/lesson/{lessonId}/assignment")
 	public List<Widget> findAllAssignmentsForLesson(@PathVariable("lessonId") int lessonId) {
 		System.out.println("here");
@@ -77,6 +107,29 @@ public class AssignmentService {
 			return outputWidgets;
 		}
 		return null;
+	}
+	
+	
+	@PostMapping("/api/lesson/{lessonId}/assignment")
+	public void createWidgets(@PathVariable("lessonId") int lessonId, @RequestBody List<Assignment> assignments) {
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		
+
+		if (data.isPresent()) {
+			Lesson les = data.get();
+			List<Widget> prevWidgets = les.getWidgets();
+			
+//			for(Widget widget: prevWidgets){
+//				 repository.deleteById(widget.getId());
+//			}
+//			
+//			repository.deleteAll();
+			for(Assignment assign: assignments){
+				assign.setLesson(les);
+				assignRepository.save(assign);
+			}
+		}
+//		return null;
 	}
 }
 
